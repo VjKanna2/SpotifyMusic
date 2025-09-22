ba6ea38a8ce0599e522d164ee7e56d27b33c6bb6
Vijay Kanna <vjkanna24@gmail.com>
2025-09-22 23:01:34 +0530

    Fix Build Failed

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import PlayerContext from './Context/PlayerContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 import '../public/styles/index.css'
 
 createRoot(document.getElementById('root')).render(
