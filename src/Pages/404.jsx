2636bac09cab93414ca4f2f980eaebaeb1306e59
Vijay Kanna <vjkanna24@gmail.com>
2025-09-27 13:33:40 +0530

    404 Styles

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -3,7 +3,7 @@ import Nav from '../Common/Nav'
 
 const Error404 = () => {
     return (
-        <div className='p-3 h-full'>
+        <div className='p-4 h-full'>
             <Nav />
             <div className='w-full h-full flex items-center justify-center text-center'>
                 <p>You're Not Added In User Management,<br /> Please Contact the Developer.</p>
